b294e06bf44e6244407a62b986ccf5475666c88e
Jose Navin JD <112388713+shvinjas@users.noreply.github.com>
2024-08-29 20:26:31 +1200

    Revert "complete"

--- client/Fuel/src/App.jsx
+++ client/Fuel/src/App.jsx
@@ -1,20 +1,17 @@
-import Home from "./pages/Home/Home";
-import Filter from "./pages/Filter/Filter"
-import Map from "./pages/Map/Map"
-import "./App.css";
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home"
+import './App.css'
 
 function App() {
+
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Filter" element={<Filter/>}/>
-        <Route path="/Map" element={<Map/>}/>
-      </Routes>
+    <Routes>
+     <Route path="/" element={<Home/>}/>
+    </Routes>
     </BrowserRouter>
+      
   );
 }
 
-export default App;
+export default App
